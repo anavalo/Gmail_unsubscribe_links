2757665bfb7067896ed463a08ea406911e5693a7
Tasos Tsournos <48796551+anavalo@users.noreply.github.com>
2019-10-08 08:12:50 +0200

    Add files via upload

---
+++ get_mails.py
@@ -0,0 +1,84 @@
+from __future__ import print_function
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+import email
+import base64
+from apiclient import errors
+
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
+
+def main():
+
+
+    def getMimeMessage(service, user_id, msg_id):
+        try:
+            message = service.users().messages().get(userId=user_id, id=msg_id,
+                                                     format='raw').execute()
+            msg_bytes = base64.urlsafe_b64decode(message['raw'].encode('ascii'))
+            msg_str = msg_bytes.decode('Latin-1')
+            mime = email.message_from_string(msg_str)
+            return msg_str    #or MIME if you use getMimeBOdy
+        except errors.HttpError as error:
+            print(f'An error occurred: {error}')
+            return None
+
+    # function to get body from MIME (at least try to)
+    def getMimeBody(mime):
+        body = ''
+        if mime.is_multipart():
+            for part in mime.walk():
+                ctype = part.get_content_type()
+                cdispo = str(part.get('Content-Disposition'))
+                if ctype == 'text/plain' and 'attachment' not in cdispo:
+                    body = part.get_payload(decode=True)
+                    body = body.decode('Latin-1')
+                    break
+        else:
+            body = mime.get_payload(decode=True)
+            body = body.decode('Latin-1')
+        return body
+
+
+    creds = None
+
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+
+
+    service = build('gmail', 'v1', credentials=creds)
+
+    msg_list_params = {
+        'userId': 'me'
+    }
+    # Call the Gmail API
+    results = service.users().messages()
+    messages_request = results.list(**msg_list_params)
+    with open('mail.txt', 'w') as f:
+        while messages_request is not None:
+            gmail_msg_list = messages_request.execute()
+            for gmail_msg in gmail_msg_list['messages']:
+                f.write(getMimeMessage(service, 'me', gmail_msg['id'])) #or call getMimeBody(getMimeMessage))
+
+
+
+
+
+if __name__ == '__main__':
+    main()
---
+++ get_unsub_links.py
@@ -0,0 +1,27 @@
+import re
+import pprint
+
+
+def get_unsub_links(txt_file):
+    text = open(txt_file).read()
+    pp = pprint.PrettyPrinter(indent=4)
+    urls = re.findall(r'(https?://\S+)',text)
+    # urls = re.findall('<a.*?href.*?>[\S]*?[uU]nsubscribe.*[\S]</a>',text)
+    duplicates = {}
+    print('Got ' + str(len(urls)) + ' URLs.')
+    for url in urls:
+        duplicates[url] = True
+    print('Got ' + str(len(duplicates)) + ' Unsubscribe URLs. (duplicates included)')
+    for k, v in duplicates.items():
+        if any(re.findall(r'(U|u)nsub', k)):
+            print(k)
+
+
+
+def main():
+    file = 'mail.txt'
+    get_unsub_links(file)
+
+
+if __name__ == '__main__':
+    main()
